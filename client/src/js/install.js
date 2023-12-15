50f32aa6c6d22fc5d5436243ea5eeb706f5ab208
Cehura-Code <134533516+Cehura-Code@users.noreply.github.com>
2023-12-14 20:07:01 -0700

    Create install.js

---
+++ client/src/js/install.js
@@ -0,0 +1,10 @@
+const butInstall = document.getElementById('buttonInstall');
+
+
+window.addEventListener('beforeinstallprompt', (event) => {});
+
+
+butInstall.addEventListener('click', async () => {});
+
+
+window.addEventListener('appinstalled', (event) => {});
